f926f23a20ee3ba286e4f80035fd3a88db2490cb
Maartje Hagens <maartjevandriesten@live.nl>
2022-11-21 20:36:18 +0100

    Working on the Challenge quiz

--- src/pages/Challenge.js
+++ src/pages/Challenge.js
@@ -1,51 +1,56 @@
 import React, {useState} from "react";
 import "../Styles/Challenge.css"
-import SingleCard from "../components/SingleCard";
-
-
-export const cardImages = [
-    { "src": "../img/helmet-1.png"},
-    { "src": "../img/potion-1.png"},
-    { "src": "../img/ring-1.png"},
-    { "src": "../img/scroll-1.png"},
-    { "src": "../img/shield-1.png"},
-    { "src": "../img/sword-1.png"}
-];
-
 
 function Challenge() {
-   
-    const [cards, setCards] = useState([]);
-    const [turns, setTurns] = useState(0);
-    const [choiceOne, setChoiceOne] = useState(null);
-    const [choiceTwo, setChoiceTwo] = useState(null);
-   
-    const shuffleCards = () => {
-        const shuffledCards = [...cardImages, ...cardImages]
-            .sort(() => Math.random() - 0.5)
-            .map((card) => ({ ...card, id: Math.random() }))
-        
-        setCards(shuffledCards)
-        setTurns(0)
+    const [grid, setGrid] = useState([
+        [0, 3, 5, 1],
+        [1, 2, 2, 4],
+        [4, 3, 5, 0],
+    ])
+    const [revealedGrid, setRevealedGrid] = useState (
+        new Array(grid.length)
+            .fill("")
+            .map(() => new Array (grid[0].length).fill(false))
+    );
+
+    const [previousClick, setPreviousClick] = useState();
+
+    function handleCardClicked(rowIndex, colIndex){
+        const clickedNumber = grid[rowIndex][colIndex];
+        const newRevealedGrid = [...revealedGrid];
+        newRevealedGrid[rowIndex][colIndex] = true;
+        newRevealedGrid[rowIndex][colIndex] = true;
+        setRevealedGrid(newRevealedGrid)
+
+        if(previousClick) {
+
+            if (previousClick !== clickedNumber) {
+                setTimeout(() => {
+                    newRevealedGrid[rowIndex][colIndex] = false;
+                    setRevealedGrid(newRevealedGrid);
+                }, 1000);
+            }
+            setPreviousClick(undefined);
+        } else {
+            setPreviousClick(previousClick);
+        }
     }
-    
-    //Add a card that a user has chosen
-    const handleChoice = (card) => {
-        choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-    }
-    
+
     return (
         <div className="Challenge">
-           <h1>Magic Match</h1>
-            <button onClick={shuffleCards}>New Game</button>
-            
-            <div className="card-grid">
-                {cards.map(card => (
-                   <SingleCard 
-                       key={card.id} 
-                       card={card}
-                        handleChoice={handleChoice}
-                   />
+            <div className="grid">
+                {grid.map((row, rowIndex) => (
+                    <div key={rowIndex} className="rowChallenge">
+                        {row.map((number, colIndex) => (
+                            <div
+                                onClick={() => handleCardClicked(rowIndex, colIndex)}
+                                key={colIndex}
+                                className="cardChallenge"
+                            >
+                                {revealedGrid[rowIndex] [colIndex] ? number : ""}
+                            </div>
+                        ))}
+                    </div>
                 ))}
             </div>
         </div>
